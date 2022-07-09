532a6017794b11f0bd6a7d57262337342d1cb955
JuanJuanHub <408351197@qq.com>
2022-07-10 03:00:43 +0800

    eureka to loadbalancer

--- SpringCloudStudy/user-service/src/main/java/com/test/controller/UserController.java
+++ SpringCloudStudy/user-service/src/main/java/com/test/controller/UserController.java
@@ -2,12 +2,14 @@ package com.test.controller;
 
 import com.test.entity.User;
 import com.test.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @RestController
 public class UserController {
 
@@ -16,6 +18,7 @@ public class UserController {
 
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
+        log.info("我被调用了!!");
         return service.getUserById(uid);
     }
 }
